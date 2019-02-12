4090060af61f5bd8dcde658ab4476b8b02a9275a
Yura Voyevidka <urik.voevidka@gmail.com>
2019-02-12 14:40:02 +0200

    Variable names fix,remove excessive tabs and spaces

--- src/ua/lviv/iot/lab1/Car.java
+++ src/ua/lviv/iot/lab1/Car.java
@@ -1,29 +1,18 @@
 package ua.lviv.iot.lab1;
 
 class Car{
-    int enginePower;
-    String mark;
-    int maxSpeed;
-    //My fields
+    private int enginePower;
+    private String mark;
+    private int maxSpeed;
     private int engineVolume;
     private int kilometrage;
 
-    static int staticField;
+    static int countOfCars = 0;
 
-    protected int protectedField1;
-    protected int protectedField2;
-
-
-    public void printStaticField1(){
-        System.out.println("Static Field: " + Car.staticField);
-    }
-
-    public void printField(){
-        System.out.println("Static Field: " + Car.staticField);
-    }
+    protected int leasingIndex;
+    protected int price;
 
     public Car(){
-
         this(0, "undefined", 0, 0, 0, 0, 0);
     }
 
@@ -31,107 +20,96 @@ class Car{
         this(enginePower, mark, maxSpeed, engineVolume, kilometrage, 0, 0);
     }
 
-    public Car(int enginePower, String mark, int maxSpeed, int engineVolume, int kilometrage, int protectedField1, int protectedField2){
+    public Car(int enginePower, String mark, int maxSpeed, int engineVolume, int kilometrage, int leasingIndex, int price){
         this.enginePower = enginePower;
         this.mark = mark;
         this.maxSpeed = maxSpeed;
         this.engineVolume = engineVolume;
         this.kilometrage = kilometrage;
-        this.protectedField1 = protectedField1;
-        this.protectedField2 = protectedField2;
+        this.leasingIndex = leasingIndex;
+        this.price = price;
     }
 
-    @Override
-    public String toString(){
-        return "\nCar{" +
-                "enginePower=" + enginePower +
-                ", mark='" + mark + '\'' +
-                ", maxSpeed=" + maxSpeed +
-                ", engineVolume=" + engineVolume +
-                ", kilometrage=" + kilometrage +
-                ", protectedField1=" + protectedField1 +
-                ", protectedField2=" + protectedField2 +
-                "}\n";
+    public void printStaticField1(){
+        System.out.println("Count of Cars (PrintStaticField): " + countOfCars);
     }
 
-    public static int getStaticField() {
-        return staticField;
+    public void printField(){
+
+        System.out.println("Count of Cars (PrintField): " + countOfCars);
     }
 
-
-
-
-    public static void setStaticField(int staticField) {
-        Car.staticField = staticField;
-    }
-
-    public int getProtectedField1() {
-        return protectedField1;
-    }
-
-    public void setProtectedField1(int protectedField1) {
-        this.protectedField1 = protectedField1;
-    }
-
-    public int getProtectedField2() {
-        return protectedField2;
-    }
-
-    public void setProtectedField2(int protectedField2) {
-        this.protectedField2 = protectedField2;
-    }
-
-    public int getEnginePower() {
+    public int getEnginePower(){
         return enginePower;
     }
 
-    public void setEnginePower(int enginePower) {
+    public void setEnginePower(int enginePower){
         this.enginePower = enginePower;
     }
 
-    public String getMark() {
+    public String getMark(){
         return mark;
     }
 
-    public void setMark(String mark) {
+    public void setMark(String mark){
         this.mark = mark;
     }
 
-    public int getMaxSpeed() {
+    public int getMaxSpeed(){
         return maxSpeed;
     }
 
-    public void setMaxSpeed(int maxSpeed) {
+    public void setMaxSpeed(int maxSpeed){
         this.maxSpeed = maxSpeed;
     }
 
-    public int getEngineVolume() {
+    public int getEngineVolume(){
         return engineVolume;
     }
 
-    public void setEngineVolume(int engineVolume) {
+    public void setEngineVolume(int engineVolume){
         this.engineVolume = engineVolume;
     }
 
-    public int getKilometrage() {
+    public int getKilometrage(){
         return kilometrage;
     }
 
-    public void setKilometrage(int kilometrage) {
+    public void setKilometrage(int kilometrage){
         this.kilometrage = kilometrage;
     }
 
-    public void resetValues (int enginePower, String mark, int maxSpeed, int engineVolume, int kilometrage, int protectedField1, int protectedField2){
+    public int getLeasingIndex(){
+        return leasingIndex;
+    }
+
+    public void setLeasingIndex(int leasingIndex){
+        this.leasingIndex = leasingIndex;
+    }
+
+    public int getPrice(){
+        return price;
+    }
+
+    public void setPrice(int price){
+        this.price = price;
+    }
+
+    public void ReserValues(int enginePower, String mark, int maxSpeed, int engineVolume, int kilometrage, int leasingIndex, int price){
         this.enginePower = enginePower;
         this.mark = mark;
         this.maxSpeed = maxSpeed;
         this.engineVolume = engineVolume;
         this.kilometrage = kilometrage;
-        this.protectedField1 = protectedField1;
-        this.protectedField2 = protectedField2;
+        this.leasingIndex = leasingIndex;
+        this.price = price;
     }
 
 
-
-
 }
+
+
+
+
+
+
--- src/ua/lviv/iot/lab1/Lab1.java
+++ src/ua/lviv/iot/lab1/Lab1.java
@@ -4,28 +4,24 @@ public class Lab1{
 
     public static void main(String[] args){
         Car carEmptyConstructor = new Car();
-        Car carAllParametersConstructor = new Car(100, "Lanos", 1000, 5, 220);
         Car carNotAllParametersConstructor = new Car(1000, "Tavriya", 10000, 10, 2000);
+        Car carAllParametersConstructor = new Car(100, "Lanos", 1000, 5, 220,1000,1);
 
+        System.out.println("Without parameters:"+carEmptyConstructor.toString());
+        System.out.println("Not all parameters:"+carNotAllParametersConstructor.toString());
+        System.out.println("All parameters:"+carAllParametersConstructor.toString());
 
-        Car.staticField = 10;
-
-        System.out.println(carEmptyConstructor.toString());
-        System.out.println(carNotAllParametersConstructor.toString());
-        System.out.println(carAllParametersConstructor.toString());
-
-
+        System.out.println("Static Field of carEmptyConstructor");
         carEmptyConstructor.printStaticField1();
         carEmptyConstructor.printField();
 
-
+        System.out.println("Static Field of carNotAllParametersConstructor");
         carNotAllParametersConstructor.printStaticField1();
         carNotAllParametersConstructor.printField();
 
-
+        System.out.println("Static Field of carAllParametersConstructor");
         carAllParametersConstructor.printStaticField1();
         carAllParametersConstructor.printField();
-
 
     }
 }
